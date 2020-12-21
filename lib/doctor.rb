35d73ed3ac33cccd0960afca6a8d67d5f939ed87
Keith Filkins <kafilkins@me.com>
2020-12-21 21:51:08 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,8 +21,7 @@ class Doctor
   end
 
   def patients
-    appointments.map do |appointment| new_appointment.patient
-    end
+    appointments.map {|appointment| appointment.patient}
   end
 
 end
