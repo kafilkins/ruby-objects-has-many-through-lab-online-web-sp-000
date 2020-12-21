37deb6f45a19d562bb53d927373d2350a432460d
Keith Filkins <kafilkins@me.com>
2020-12-21 22:57:00 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -19,7 +19,6 @@ class Patient
   def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
     end
-  end
 
   def doctors
     appointments.collect {|appointment| appointment.doctor}
