9c4f4b0f37a2d0931905b80cd7531eead0be37d2
Keith Filkins <kafilkins@me.com>
2020-12-21 22:45:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,6 +15,7 @@ class Doctor
 
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
+  end
 
   def new_appointment(date, patient)
      appointment = Appointment.new(patient, date, self)
