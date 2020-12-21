26ce610709a856e1f112a3d91efe0fcb067f9e2c
Keith Filkins <kafilkins@me.com>
2020-12-21 23:02:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,7 +18,7 @@ class Doctor
   end
 
   def new_appointment(date, patient)
-     appointment = Appointment.new(patient, date, self)
+     appointment = Appointment.new(date, patient, self)
      @appointments << appointment
      appointment
   end
