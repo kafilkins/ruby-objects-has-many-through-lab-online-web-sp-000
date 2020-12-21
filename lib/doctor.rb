117304990d6e07c57473f96c37d31b72b07c0626
Keith Filkins <kafilkins@me.com>
2020-12-21 23:04:29 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,11 +1,10 @@
 class Doctor
 
-  attr_accessor :name, :appointment
+  attr_accessor :name, 
   @@all = []
 
   def initialize(name)
     @name = name
-    @appointment = appointment
     @@all << self
   end
 
